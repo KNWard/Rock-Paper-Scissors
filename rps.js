c92e16149031e5aecaab756d21b56b743534d06b
Kendra <92537842+KNWard@users.noreply.github.com>
2025-01-13 13:28:48 -0500

    Wrote  computer choice and human choice functions.

--- rps.js
+++ rps.js
@@ -1,8 +1,31 @@
 function getComputerChoice() {
- 
+ let numChoice = Math.floor(Math.random() * 3);
+
+ switch (numChoice) {
+  case 0:
+   return "rock";
+  case 1:
+   return "paper";
+  case 2:
+   return "scissors";
+ }
 }
-// let x = Math.random();
-// console.log(x);
+
+console.log(getComputerChoice());
 
 
+function getHumanChoice() {
+ let choice = prompt("Choose one: Rock, Paper, or Scissors");
 
+ if (choice.toLowerCase() === "rock") {
+  console.log("rock");
+ } else if (choice.toLowerCase() === "paper") {
+  console.log("paper");
+ } else if (choice.toLowerCase() === "scissors") {
+  console.log("scissors");
+ } else {
+  console.log("Please make a choice.")
+ }
+}
+
+console.log(getHumanChoice());
